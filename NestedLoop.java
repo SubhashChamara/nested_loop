45b84d27fcf6372a3a6d62d593901ea7a8772d7b
Subhash Chamara <udayanga.subhash94@gmail.com>
2022-08-30 17:04:48 +0530

    new

---
+++ NestedLoop.java
@@ -0,0 +1,13 @@
+public class NestedLoop {
+    public static void main(String args[]){
+
+
+        for(int i=1 ; i<5;i++){
+            for(int x = i;x>0;x--){
+                System.out.print("* ");
+            }
+            System.out.println("");
+
+        }
+    }
+}
